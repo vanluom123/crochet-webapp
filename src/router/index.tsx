27e6cd9a5d83981ac1d4e2c175a06914538c9e2f
Charles Phan <phanvanluom97bd@gmail.com>
2024-07-18 20:15:51 +0700

    setup vite

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import Contact from '../pages/contact/Contact';
 
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    
+    path: '/contact',
+    element: <Contact />,
+  }
+
+]);
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const Router = () => {
