979a72780272372c275b05f65df3f0e1348078b7
Charles Phan <phanvanluom97bd@gmail.com>
2024-07-18 18:20:08 +0700

    setup to auto deploy

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import './index.css'
 import { Router } from './router'
@@ -8,8 +8,8 @@ import { Router } from './router'
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
         <Router />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
