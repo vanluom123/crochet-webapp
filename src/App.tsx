4983faece0cf9c71f19d48e441a8713bb0a2785f
Charles Phan <phanvanluom97bd@gmail.com>
2024-07-18 20:33:59 +0700

    update img path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import tieuphuong from '/logo192.png'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 
@@ -11,7 +11,7 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={tieuphuong} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
