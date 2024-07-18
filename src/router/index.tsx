11b046a0e17700cc145778f5befaaa0fb80450cf
Charles Phan <phanvanluom97bd@gmail.com>
2024-07-18 20:29:38 +0700

    config vite deploy

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,28 @@
 import { Routes, Route, createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import Contact from '../pages/contact/Contact';
+import App from '../App';
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    
-    path: '/contact',
-    element: <Contact />,
-  }
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
 
-]);
+      },
+      {
+
+        path: '/contact',
+        element: <Contact />,
+      }
+    ]
+  },
+  
+], {basename: '/crochet-webapp/'}
+);
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const Router = () => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages';  
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    ghPages()
-  ],
+  plugins: [react()  ],
   base: "/crochet-webapp/",
   
 })
