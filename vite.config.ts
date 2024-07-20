15a1fd2bbc80836c6c9354c13c6825ba91543082
Charles Phan <phanvanluom97bd@gmail.com>
2024-07-20 14:15:25 +0700

    Deploy ssr with firebase functions

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/crochet-webapp/",
+// })
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/crochet-webapp/",
+//   build: {
+//     ssr: 'src/entry-server.jsx',
+//     rollupOptions: {
+//       input: 'src/entry-server.jsx',
+//       output: {
+//         entryFileNames: 'entry-server.js',
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
-  base: "/crochet-webapp/",
+  build: {
+    ssr: true,
+    outDir: 'dist/server',
+    rollupOptions: {
+      input: 'src/entry-server.tsx',
+    },
+  },
 })
